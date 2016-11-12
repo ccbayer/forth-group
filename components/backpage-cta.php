e2bca6277b605503d93365b74a90b2010d69ac83
Colin Bayer <ccbayer@gmail.com>
2016-11-12 10:59:10 -0800

    CCB various updates

--- components/backpage-cta.php
+++ components/backpage-cta.php
@@ -3,31 +3,22 @@
 	if($show):
 		$link = get_field('button_link_type') === 'Internal' ? get_permalink(get_field('button_link_internal')) : get_field('button_link_external');
 		$target = get_field('button_link_type') === 'Internal' ? '_self' : '_blank';
-		$btnClass = 'btn';
-		$labelWidth = 7;
-		$btnWidth = 5;
-		if(get_field('secondary_bottom_cta')):
-			$btnClass = $btnClass.' btn2';
-			$labelWidth = 6;
-			$btnWidth = 6;
-		endif;
-
 ?>
 <div class="bg-light-green bp-cta-wrapper">
     <div class="container">
         <div class="row">
-        	<div class="col-md-<?php echo $labelWidth ?> lbl-wrapper">
+        	<div class="col-md-12 lbl-wrapper">
         		<h3><?php the_field('bottom_cta_text'); ?></h2>
         	</div>
-        	<div class="col-md-<?php echo $btnWidth ?> btn-wrapper">
-            	<a class="<?php echo $btnClass ?>" href="<?php echo $link; ?>" target=<?php echo $target ?>><?php the_field('bottom_cta_button_label'); ?> &raquo;</a>
+        	<div class="col-md-12 btn-wrapper">
+            	<a class="btn" href="<?php echo $link; ?>" target=<?php echo $target ?>><?php the_field('bottom_cta_button_label'); ?> &raquo;</a>
 							<?php
 								// is there a secondary CTA?
 								if(get_field('secondary_bottom_cta')):
 									$secondarylink = get_field('secondary_bottom_cta_link_type') === 'Internal' ? get_permalink(get_field('secondary_bottom_cta_internal_link')) : get_field('secondary_bottom_cta_external_link');
 									$secondarytarget = get_field('secondary_bottom_cta_link_type') === 'Internal' ? '_self' : '_blank';
 							?>
-							<a class="<?php echo $btnClass ?>" href="<?php echo $secondarylink; ?>" target=<?php echo $secondarytarget ?>><?php the_field('secondary_bottom_cta_button_label'); ?> &raquo;</a>
+							<a class="btn" href="<?php echo $secondarylink; ?>" target=<?php echo $secondarytarget ?>><?php the_field('secondary_bottom_cta_button_label'); ?> &raquo;</a>
 							<?php
 								endif;
 							?>
--- inc/enqueue.php
+++ inc/enqueue.php
@@ -6,7 +6,12 @@
  */
 
 function understrap_scripts() {
-    wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/css/theme.min.css', array(), '0.4.7');
+    if($_SERVER['HTTP_HOST'] === 'dev.forth-grp.com'):
+      wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/css/theme.css', array(), '0.4.7');
+    else:
+      wp_enqueue_style( 'understrap-styles', get_stylesheet_directory_uri() . '/css/theme.min.css', array(), '0.4.7');
+    endif; 
+
     wp_enqueue_script('jquery');
     wp_enqueue_script( 'understrap-scripts', get_template_directory_uri() . '/js/theme.min.js', array(), '0.4.7', true );
 
