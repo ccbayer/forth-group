8fb3fa081784e9309c3967df4b25958388b101cc
Colin Bayer <ccbayer@gmail.com>
2016-11-25 16:05:25 -0800

    CB responsive tabs cleanup: consolidate functions, fix scrollto on mobile

--- components/sidenav-panel.php
+++ components/sidenav-panel.php
@@ -10,19 +10,19 @@
 		                for($i = 0; $i< $count; $i++) {
 			                $n = $i + 1;
 			                $class = $i === 0 ? 'active' : '';
-							echo '<li class="'.$class.'"><a href="#content-'.$n.'" class="tk">'.$tabs[$i]['tab_label'].'</a> <span class="fa fa-chevron-right" aria-hidden="true"></span></li>';
+							echo '<li class="'.$class.'"><a href="#content-'.$n.'" class="tab-switch tk">'.$tabs[$i]['tab_label'].'</a> <span class="fa fa-chevron-right" aria-hidden="true"></span></li>';
 		                }
 		                ?>
 	                </ul>
                 </div>
-                <div class="col-md-8 col-sm-12">
+                <div class="col-md-8 col-sm-12 main-tab-content-wrapper">
                         <!-- content item 1-->
                         <?php
 							for($i = 0; $i< $count; $i++) {
 								$n = $i + 1;
 								$class = $i === 0 ? 'active' : '';
 							?>
-              <a href="#content-<?php echo $n; ?>" class="tk responsive-tab-trigger <?php echo $class ?>"><?php echo $tabs[$i]['tab_label']; ?><span class="fa fa-chevron-right" aria-hidden="true"></span></a>
+              <a href="#content-<?php echo $n; ?>" class="tk tab-switch <?php echo $class ?>"><?php echo $tabs[$i]['tab_label']; ?><span class="fa fa-chevron-right" aria-hidden="true"></span></a>
               <div class="content-wrapper">
 							<div class="content-item <?php echo $class; ?>" id="content-<?php echo $n; ?>">
 								<?php echo $tabs[$i]['tab_content']; ?>
