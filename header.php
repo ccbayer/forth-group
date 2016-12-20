eff65d0a495f48db74f36330c535043ad26bea95
Colin Bayer <ccbayer@gmail.com>
2016-12-19 17:11:04 -0800

    CB add analytics to head

--- header.php
+++ header.php
@@ -19,6 +19,15 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <!-- typekit -->
 <script src="https://use.typekit.net/awh1ivy.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-65334141-1', 'auto');
+  ga('send', 'pageview');
+</script>
 <?php wp_head(); ?>
 </head>
 
