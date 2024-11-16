bd0c573d13eb9eb8ba8a0bfb19ea7d60d4bdcadf
Colin Bayer <ccbayer@Colins-MacBook-Pro.local>
2024-11-16 09:38:11 -0800

    First cut at updating ACF renderer

--- partials/home-testimonials.php
+++ partials/home-testimonials.php
@@ -1,37 +1,42 @@
 <?php
-  $testimonials = $args['testimonials']; 
-  if (!empty($testimonials)):
+$testimonials = $args['testimonials'];
+if (!empty($testimonials)):
   $testominal_size = sizeof($testimonials);
 ?>
   <div class="col-md-7">
     <div class="testimonial-inner-wrapper">
-      <?php 
-        for($i = 0; $i < $testominal_size; $i++):
-          $active = $i === 0 ? 'active' : '';
-        ?>
-          <blockquote id="testimonial-<?=$i ?>" class="<?= $active; ?>">
-            <p><?= $testimonials[$i]['testimonial_copy']; ?></p>
-            <p class="bq-footer"><?= $testimonials[$i]['testimonial_author']; ?></p>
-              </blockquote>
-          <?php
-          endfor;
-        ?>
-      </div>
-    <?php if($testominal_size > 1): ?>
+      <?php
+      for ($i = 0; $i < $testominal_size; $i++):
+        $active = $i === 0 ? 'active' : '';
+      ?>
+        <blockquote id="testimonial-<?= $i ?>" class="<?= $active; ?>">
+          <p><?= acf_esc_html($testimonials[$i]['testimonial_copy']); ?></p>
+          <p class="bq-footer">
+            <?= acf_esc_html($testimonials[$i]['testimonial_author']); ?></p>
+        </blockquote>
+      <?php
+      endfor;
+      ?>
+    </div>
+    <?php if ($testominal_size > 1): ?>
       <div class="testimonial-nav">
         <ul>
-        <?php
-          for($i = 0; $i < $testominal_size; $i++):
-          $active = $i === 0 ? 'active' : '';
-        ?>
-          <li>
-            <button data-target="#testimonial-<?= $i ?>" class="<?= $active ?>" aria-label="View Testimonial <?= $i ?>" title="View Testimonial <?= $i ?>">
-              <span class="sr-only">View Testimonial <?= $i ?></span>
-            </button>
-          </li>
-        <?php endfor; ?>
+          <?php
+          for ($i = 0; $i < $testominal_size; $i++):
+            $active = $i === 0 ? 'active' : '';
+          ?>
+            <li>
+              <button
+                data-target="#testimonial-<?= $i ?>"
+                class="<?= esc_attr($active) ?>"
+                aria-label="View Testimonial <?= esc_attr($i) ?>"
+                title="View Testimonial <?= esc_attr($i) ?>">
+                <span class="sr-only">View Testimonial <?= acf_esc_html($i); ?></span>
+              </button>
+            </li>
+          <?php endfor; ?>
         </ul>
       </div>
     <?php endif; ?>
-    </div>
-  <?php endif; ?>
+  </div>
+<?php endif; ?>
