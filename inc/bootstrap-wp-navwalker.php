b73b604a2555c5f8a527f600c709e0c2951ad19f
Colin Bayer <ccbayer@Colins-MacBook-Pro.local>
2024-11-17 12:08:55 -0800

    Address aria-label

--- inc/bootstrap-wp-navwalker.php
+++ inc/bootstrap-wp-navwalker.php
@@ -92,7 +92,7 @@ class wp_bootstrap_navwalker extends Walker_Nav_Menu
       $id = $id ? ' id="' . esc_attr($id) . '"' : '';
       $output .= $indent . '<li' . $id . $value . $class_names . '>';
       $atts = array();
-      $atts['aria-title']  = ! empty($item->title)  ? $item->title  : '';
+      $atts['aria-label']  = ! empty($item->title)  ? $item->title  : '';
       $atts['target'] = ! empty($item->target)  ? $item->target  : '';
       $atts['rel']    = ! empty($item->xfn)    ? $item->xfn  : '';
       // If item has_children add atts to a.
